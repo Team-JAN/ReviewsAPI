9533eafb7f07e6f2d3d4d102023f13beea361e8f
Alex Ho <alexanderkho92@gmail.com>
2020-01-09 13:59:35 -0700

    update k6 file

--- loadTesting/getReviews_k6.js
+++ loadTesting/getReviews_k6.js
@@ -2,17 +2,25 @@ import http from "k6/http";
 import { sleep } from "k6";
 
 // const products = ['2', '11', '101', '1001', '10001', '100001', '1000001'];
+const servers = ['13.58.176.208:5000', '13.59.130.62:5000', '18.188.132.96:5000'];
 
 export let options = {
-  // vus: 5,
+  vus: 100,
   duration: "30s",
-  rps: 100
+  rps: 1000
 };
 
 export default function() {
-  // const product = products[Math.floor(Math.random() * 7)];
-  const product = Math.floor(Math.random() * 1000000);
-  // const product = 12345;
+  const product = Math.floor(Math.random() * 1000000 + 1);
+
+  //through ELB
   http.get(`http://sdcLB-1824053846.us-east-2.elb.amazonaws.com:80/reviews/${product}/list`);
+
+  //through single server
+  // http.get(`http://18.188.132.96:5000/reviews/${product}/list`);
+
+  //thru random server
+  // const server = servers[Math.floor(Math.random() * 3)];
+  // http.get(`http://${server}/reviews/${product}/list`);
   // sleep(1);
 };
